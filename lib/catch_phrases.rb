f591c8518acdd7d2cf4de0c19580bea9544bbaf9
Sebastien Dornel <kai_dornel@hotmail.com>
2020-02-16 19:20:39 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,7 +4,18 @@ def mario
 end
 
 def toadstool
-  phrase = ""
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
+  puts status
+end
+
+def link
+  said = "It's Dangerous To Go Alone! Take This."
+  puts said
+end
+
+def any_phrase(phrase)
+  puts phrase
+end
 
 
 
